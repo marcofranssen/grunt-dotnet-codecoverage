52d8733df37f94bd6804aefbd258fb0c5779ffef
Jean Rodriguez <jean_rodriguez@ultimatesoftware.com>
2015-02-20 13:48:55 -0500

    Added additional options: filter, excludebyattribute and targetargs.

--- test/opencover_test.js
+++ test/opencover_test.js
@@ -35,19 +35,33 @@ exports.codecoverage = {
 
         test.done();
     },
+    targetargs: function(test) {
+        test.expect(2);
+
+        var test_output_xml_exists = grunt.file.exists('reports/TestResults.xml');
+        var test_output_html_exists = grunt.file.exists('reports/TestResults.html');
+        test.ok(test_output_xml_exists, 'reports/TestResults.xml');
+        test.ok(test_output_html_exists, 'reports/TestResults.html');
+
+        test.done();
+    },
     reportgenerator: function(test) {
-        test.expect(5);
+        test.expect(7);
 
         var index_html_exists = grunt.file.exists('reports/codecoverage/index.htm');
         var report_css_exists = grunt.file.exists('reports/codecoverage/report.css');
         var combined_js_exists = grunt.file.exists('reports/codecoverage/combined.js');
         var mycode_calculator_html_exists = grunt.file.exists('reports/codecoverage/MyCode_Calculator.htm');
+        var myspecs_html_does_not_exists = !grunt.file.isFile('reports/codecoverage/MySpecs_When_adding_using_the_calculator.htm');
+        var excludedcode_excludedclass_html_not_exists = !grunt.file.isFile('reports/codecoverage/ExcludedCode_ExcludedClass.htm');
         var summary_xml_exists = grunt.file.exists('reports/codecoverage/summary.xml');
 
         test.ok(index_html_exists, 'reports/codecoverage/index.xml should exist');
         test.ok(report_css_exists, 'reports/codecoverage/report.css should exist');
         test.ok(combined_js_exists, 'reports/codecoverage/combined.js should exist');
         test.ok(mycode_calculator_html_exists, 'reports/codecoverage/MyCode_Calculator.htm should exist');
+        test.ok(myspecs_html_does_not_exists, 'reports/codecoverage/MySpecs_When_adding_using_the_calculator.htm should not exist');
+        test.ok(excludedcode_excludedclass_html_not_exists, 'reports/codecoverage/ExcludedCode_ExcludedClass.htm should not exist');
         test.ok(summary_xml_exists, 'reports/codecoverage/summary.xml should exist');
 
         test.done();
